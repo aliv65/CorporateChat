4a78a688ad3f030510be1366679b284463922b8b
Александр Иванин <ivanin@sebbia.com>
2017-11-05 13:13:42 +0300

    + файлы проекта

---
+++ CorporateChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CorporateChat
+//
+//  Created by Александр Иванин on 05.11.17.
+//  Copyright © 2017 yard.band. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
